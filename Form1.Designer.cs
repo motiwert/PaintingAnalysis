7dedb100fdc1768b5853a63c07505a081dfbc819
motiwert <moti.wertheimer@philips.com>
2025-07-08 11:33:45 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace DrawingDiagnosisTool
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button btnNotPresent;
+        private System.Windows.Forms.Button btnPresent;
+        private System.Windows.Forms.Button btnEmphasized;
+        private System.Windows.Forms.Button btnMissing;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.TextBox txtXlsInput;
+        private System.Windows.Forms.TextBox txtTxtOutput;
+        private System.Windows.Forms.Label lblXlsInput;
+        private System.Windows.Forms.Label lblTxtOutput;
+        private System.Windows.Forms.Button btnShowTable;
+        private System.Windows.Forms.DataGridView dgvSummary;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnNotPresent = new System.Windows.Forms.Button();
+            this.btnPresent = new System.Windows.Forms.Button();
+            this.btnEmphasized = new System.Windows.Forms.Button();
+            this.btnMissing = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.txtXlsInput = new System.Windows.Forms.TextBox();
+            this.txtTxtOutput = new System.Windows.Forms.TextBox();
+            this.lblXlsInput = new System.Windows.Forms.Label();
+            this.lblTxtOutput = new System.Windows.Forms.Label();
+            this.btnShowTable = new System.Windows.Forms.Button();
+            this.dgvSummary = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSummary)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblQuestion.Location = new System.Drawing.Point(30, 20);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(720, 60);
+            this.lblQuestion.Text = "פרט לצפייה";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnNotPresent
+            // 
+            this.btnNotPresent.Location = new System.Drawing.Point(620, 100);
+            this.btnNotPresent.Name = "btnNotPresent";
+            this.btnNotPresent.Size = new System.Drawing.Size(130, 40);
+            this.btnNotPresent.Text = "לא מופיע";
+            this.btnNotPresent.UseVisualStyleBackColor = true;
+            this.btnNotPresent.Click += new System.EventHandler(this.btnNotPresent_Click);
+            // 
+            // btnPresent
+            // 
+            this.btnPresent.Location = new System.Drawing.Point(470, 100);
+            this.btnPresent.Name = "btnPresent";
+            this.btnPresent.Size = new System.Drawing.Size(130, 40);
+            this.btnPresent.Text = "מופיע";
+            this.btnPresent.UseVisualStyleBackColor = true;
+            this.btnPresent.Click += new System.EventHandler(this.btnPresent_Click);
+            // 
+            // btnEmphasized
+            // 
+            this.btnEmphasized.Location = new System.Drawing.Point(320, 100);
+            this.btnEmphasized.Name = "btnEmphasized";
+            this.btnEmphasized.Size = new System.Drawing.Size(130, 40);
+            this.btnEmphasized.Text = "בולט";
+            this.btnEmphasized.UseVisualStyleBackColor = true;
+            this.btnEmphasized.Click += new System.EventHandler(this.btnEmphasized_Click);
+            // 
+            // btnMissing
+            // 
+            this.btnMissing.Location = new System.Drawing.Point(170, 100);
+            this.btnMissing.Name = "btnMissing";
+            this.btnMissing.Size = new System.Drawing.Size(130, 40);
+            this.btnMissing.Text = "חסר";
+            this.btnMissing.UseVisualStyleBackColor = true;
+            this.btnMissing.Click += new System.EventHandler(this.btnMissing_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(30, 170);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(150, 40);
+            this.btnExport.Text = "שמור סיכום";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lblXlsInput
+            // 
+            this.lblXlsInput.Location = new System.Drawing.Point(620, 170);
+            this.lblXlsInput.Name = "lblXlsInput";
+            this.lblXlsInput.Size = new System.Drawing.Size(120, 25);
+            this.lblXlsInput.Text = "קובץ קלט XLS:";
+            this.lblXlsInput.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtXlsInput
+            // 
+            this.txtXlsInput.Location = new System.Drawing.Point(400, 170);
+            this.txtXlsInput.Name = "txtXlsInput";
+            this.txtXlsInput.Size = new System.Drawing.Size(210, 31);
+            this.txtXlsInput.Text = "מילון ציורים גירסה 2.xlsx";
+            // 
+            // lblTxtOutput
+            // 
+            this.lblTxtOutput.Location = new System.Drawing.Point(620, 210);
+            this.lblTxtOutput.Name = "lblTxtOutput";
+            this.lblTxtOutput.Size = new System.Drawing.Size(120, 25);
+            this.lblTxtOutput.Text = "קובץ פלט TXT:";
+            this.lblTxtOutput.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtTxtOutput
+            // 
+            this.txtTxtOutput.Location = new System.Drawing.Point(400, 210);
+            this.txtTxtOutput.Name = "txtTxtOutput";
+            this.txtTxtOutput.Size = new System.Drawing.Size(210, 31);
+            this.txtTxtOutput.Text = "סיכום.txt";
+            // 
+            // btnShowTable
+            // 
+            this.btnShowTable.Location = new System.Drawing.Point(30, 220);
+            this.btnShowTable.Name = "btnShowTable";
+            this.btnShowTable.Size = new System.Drawing.Size(150, 40);
+            this.btnShowTable.Text = "הצג טבלה";
+            this.btnShowTable.UseVisualStyleBackColor = true;
+            this.btnShowTable.Click += new System.EventHandler(this.btnShowTable_Click);
+            // 
+            // dgvSummary
+            // 
+            this.dgvSummary.Location = new System.Drawing.Point(30, 280);
+            this.dgvSummary.Name = "dgvSummary";
+            this.dgvSummary.Size = new System.Drawing.Size(710, 200);
+            this.dgvSummary.ReadOnly = true;
+            this.dgvSummary.AllowUserToAddRows = false;
+            this.dgvSummary.AllowUserToDeleteRows = false;
+            this.dgvSummary.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 500);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnNotPresent);
+            this.Controls.Add(this.btnPresent);
+            this.Controls.Add(this.btnEmphasized);
+            this.Controls.Add(this.btnMissing);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.lblXlsInput);
+            this.Controls.Add(this.txtXlsInput);
+            this.Controls.Add(this.lblTxtOutput);
+            this.Controls.Add(this.txtTxtOutput);
+            this.Controls.Add(this.btnShowTable);
+            this.Controls.Add(this.dgvSummary);
+            this.Name = "Form1";
+            this.Text = "אבחון ציורים";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSummary)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace DrawingDiagnosisTool
+{
+    public partial class Form1 : Form
+    {
+        private List<DrawingItem> items = new();
+        private int currentIndex = 0;
+        private Dictionary<DrawingItem, string> answers = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Use the input TextBox for the XLS file path
+            LoadData(txtXlsInput.Text);
+            ShowCurrentItem();
+        }
+
+        private void LoadData(string path)
+        {
+            items.Clear();
+            var workbook = new XLWorkbook(path);
+            var worksheet = workbook.Worksheet("גיליון1");
+            foreach (var row in worksheet.RowsUsed().Skip(1))
+            {
+                items.Add(new DrawingItem
+                {
+                    Topic = row.Cell(1).GetString(),
+                    Subtopic = row.Cell(2).GetString(),
+                    Detail = row.Cell(3).GetString(),
+                    Explanation = row.Cell(4).GetString(),
+                    Category = row.Cell(5).GetString()
+                });
+            }
+        }
+
+        private void ShowCurrentItem()
+        {
+            if (currentIndex < items.Count)
+            {
+                var item = items[currentIndex];
+                lblQuestion.Text = $"{item.Topic} > {item.Subtopic} > {item.Detail}";
+            }
+            else
+            {
+                MessageBox.Show("סיימת את כל הפרטים. כעת תוכל לייצא סיכום.");
+            }
+        }
+
+        private void RecordAnswer(string answer)
+        {
+            if (currentIndex < items.Count)
+            {
+                answers[items[currentIndex]] = answer;
+                currentIndex++;
+                ShowCurrentItem();
+            }
+        }
+
+        private void btnNotPresent_Click(object sender, EventArgs e) => RecordAnswer("לא מופיע");
+        private void btnPresent_Click(object sender, EventArgs e) => RecordAnswer("מופיע");
+        private void btnEmphasized_Click(object sender, EventArgs e) => RecordAnswer("בולט");
+        private void btnMissing_Click(object sender, EventArgs e) => RecordAnswer("חסר");
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            var grouped = answers
+                .Where(p => p.Value != "לא מופיע")
+                .GroupBy(p => p.Key.Category)
+                .ToDictionary(g => g.Key, g => g.Select(p => $"- {p.Key.Detail}: {p.Key.Explanation} ({p.Value})"));
+
+            // Use the output TextBox for the TXT file path
+            string outputPath = txtTxtOutput.Text;
+            using (var writer = new StreamWriter(outputPath, false, System.Text.Encoding.UTF8))
+            {
+                foreach (var category in grouped.Keys)
+                {
+                    writer.WriteLine($"תחום: {category}");
+                    foreach (var line in grouped[category])
+                        writer.WriteLine(line);
+                    writer.WriteLine();
+                }
+            }
+
+            MessageBox.Show($"הסיכום נשמר כקובץ {outputPath}");
+        }
+
+        private void btnShowTable_Click(object sender, EventArgs e)
+        {
+            // Show the summary as a table in the DataGridView
+            var table = new DataTable();
+            table.Columns.Add("קטגוריה");
+            table.Columns.Add("נושא");
+            table.Columns.Add("תת נושא");
+            table.Columns.Add("פרט");
+            table.Columns.Add("הסבר");
+            table.Columns.Add("סטטוס");
+
+            foreach (var entry in answers.Where(p => p.Value != "לא מופיע"))
+            {
+                table.Rows.Add(
+                    entry.Key.Category,
+                    entry.Key.Topic,
+                    entry.Key.Subtopic,
+                    entry.Key.Detail,
+                    entry.Key.Explanation,
+                    entry.Value
+                );
+            }
+            dgvSummary.DataSource = table;
+        }
+    }
+
+    public class DrawingItem
+    {
+        public string Topic { get; set; }
+        public string Subtopic { get; set; }
+        public string Detail { get; set; }
+        public string Explanation { get; set; }
+        public string Category { get; set; }
+    }
+}
